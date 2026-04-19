781a0f0fb8854bdbdd6cbbcda8f43fdf305761d4
Ndudi <ndudiokehi@gmail.com>
2026-04-19 11:50:04 +0100

    Fixed Vite Config.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,8 @@
-/* eslint-env node */
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, "", "");
   const apiTarget = env.VITE_API_BASE_URL || "http://localhost:8000";
 
   return {
